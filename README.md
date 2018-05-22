0085cdf921044cb2e518bcc4b9572be07f205f81
Renan Consalter <renan@bonesmart.tech>
2018-05-22 17:48:55 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# H-Switch Plugin for Homebridge
+
+Exemplo de configuração de um dispositivo H-Switch no config.json do Homebridge:
+
+ ```
+ "accessories": [
+   {
+       "accessory": "H-Switch",
+       "name": "Informe um nome para o seu dispositivo",
+       "ip" : "Informe o endereço de rede do seu dispositivo",
+       "serialnumber" : "Insira aqui o número de série do seu dispositivo"
+   }
+ ],
+
+```
+
+> Obs.: O número de série é opcional
+
+[![](https://scontent.fpoa10-1.fna.fbcdn.net/v/t1.0-9/29683359_2008233752836223_8865180325666098214_n.png?_nc_cat=0&oh=8731c63f4c6725c6f421da3e4138b791&oe=5B91F9C9)](http://bonesmart.tech)
---
+++ index.js
@@ -0,0 +1,65 @@
+var request = require("request");
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+
+  homebridge.registerAccessory("homebridge-bonesmart-hswitch", "H-Switch", HSwitchAccessory);
+}
+
+function HSwitchAccessory(log, config) {
+  this.log = log;
+  this.name = config["name"];
+  this.ip = config["ip"];
+  this.manufacturer = config["manufacturer"] || "Bone Smart Technology";
+  this.model = config["model"] || "H-Switch";
+  this.serialnumber = config["serialnumber"] || "BSTHSXXXXX";
+  this.firmwarerevision = config["firmwarerevision"] || "1.0.0";
+}
+
+HSwitchAccessory.prototype.identify = function(callback) {
+  this.log('O sistema identificou um request!');
+  callback();
+};
+
+HSwitchAccessory.prototype.getPowerState = function(callback) {
+  this.log("Recuperando estado do acessório H-Switch...");
+  request.get({
+    url: 'http://' + this.ip + '/status'
+  }, function(err, response, body) {
+    var status = body == '1' ? 1 : 0;
+    callback(null, status);
+  }.bind(this));
+}
+
+HSwitchAccessory.prototype.setPowerState = function(state, callback) {
+  var url = state ? "1": "0";
+  this.log("Alterando estado do acessório H-Switch...");
+  request.get({
+    url: 'http://' + this.ip + '/relay?state=' + url
+  }, function(err, response, body) {
+  callback(null, state);
+  }.bind(this));
+  this.log("O status do dispositivo foi alterado para " + status);  
+},
+
+HSwitchAccessory.prototype.getServices = function() {
+
+  var informationService = new Service.AccessoryInformation();
+
+  informationService
+    .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+    .setCharacteristic(Characteristic.Model, this.model)
+    .setCharacteristic(Characteristic.SerialNumber, this.serialnumber)
+    .setCharacteristic(Characteristic.FirmwareRevision, this.firmwarerevision);
+
+  var lightbulbService = new Service.Lightbulb(this.name);
+
+  lightbulbService
+    .getCharacteristic(Characteristic.On)
+    .on('get', this.getPowerState.bind(this))
+    .on('set', this.setPowerState.bind(this));
+
+  return [informationService, lightbulbService];
+}
